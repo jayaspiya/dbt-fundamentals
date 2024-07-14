2d5c11b585fb5f9592a7d4afbef59fc936528a75
Jayas Piya <jayaspiya52@gmail.com>
2024-07-14 16:59:43 +0000

    Refactored fct_orders and dim_customers

--- models/marts/finance/fct_orders.sql
+++ models/marts/finance/fct_orders.sql
@@ -1,13 +1,30 @@
-with payments as (
-    select * from {{ ref('stg_stripe__payments') }}
+with orders as  (
+    select * from {{ ref ('stg_jaffle_shop__orders' )}}
 ),
-orders as (
-    select * from {{ ref('stg_jaffle_shop__orders') }}
+
+payments as (
+    select * from {{ ref ('stg_jaffle_shop__payments') }}
+),
+
+order_payments as (
+    select
+        order_id,
+        sum (case when status = 'success' then amount end) as amount
+
+    from payments
+    group by 1
+),
+
+ final as (
+
+    select
+        orders.order_id,
+        orders.customer_id,
+        orders.order_date,
+        coalesce (order_payments.amount, 0) as amount
+
+    from orders
+    left join order_payments using (order_id)
 )
-select
-    orders.order_id,
-    orders.customer_id,
-    payments.amount
-from payments
-join orders
-on payments.order_id = orders.order_id
+
+select * from final
